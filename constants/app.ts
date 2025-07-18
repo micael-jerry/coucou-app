91b83934c4919bbca5a86c791882978ffca17e66
Micael Jerry <fidimalala.mj@gmail.com>
2025-07-18 12:24:56 +0300

    fix: resolve

--- constants/app.ts
+++ constants/app.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'Coucou';
-export const APP_YEAR = 2025;
+export const APP_YEAR = new Date().getFullYear();
 export const APP_CREATOR = 'Micael Jerry';
 export const APP_CREATOR_URL = 'https://micael-jerry.github.io/portfolio-micael-jerry/';
