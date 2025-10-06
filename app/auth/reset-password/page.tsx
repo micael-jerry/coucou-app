dcc26d332ade1d46a6a1eb22b370e1f4ff7745e0
Micael Jerry <fidimalala.mj@gmail.com>
2025-10-06 13:31:07 +0300

    chore: reset password

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,20 +1,10 @@
 import AuthCard from '@/components/features/auth/auth-card';
 import ResetPasswordForm from '@/components/features/auth/reset-password/reset-password-form';
-import { ROUTES } from '@/src/constants/routes';
-import { redirect } from 'next/navigation';
 
-interface ResetPasswordProps {
-	searchParams: Promise<{ token?: string }>;
-}
-
-export default async function ResetPassword(props: ResetPasswordProps) {
-	const searchParams = await props.searchParams;
-
-	if (!searchParams.token) redirect(ROUTES.HOME);
-
+export default async function ResetPassword() {
 	return (
 		<AuthCard title="Reset password" description="Please enter a new password">
-			<ResetPasswordForm token={searchParams.token} />
+			<ResetPasswordForm />
 		</AuthCard>
 	);
 }
--- components/features/auth/reset-password/reset-password-form.tsx
+++ components/features/auth/reset-password/reset-password-form.tsx
@@ -1,9 +1,97 @@
 'use client';
 
-interface ResetPasswordFormProps {
-	token: string;
-}
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { apiClient } from '@/lib/api/api-client';
+import { ROUTES } from '@/src/constants/routes';
+import { ResetPasswordFormSchema } from '@/src/schema/auth/reset-password-form-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
 
-export default function ResetPasswordForm({ token }: ResetPasswordFormProps) {
-	return <div></div>;
+export default function ResetPasswordForm() {
+	const [error, setError] = useState<string | null>(null);
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const [isLoading, setIsLoading] = useState(false);
+	const resetPasswordForm = useForm<z.infer<typeof ResetPasswordFormSchema>>({
+		resolver: zodResolver(ResetPasswordFormSchema),
+		defaultValues: {
+			newPassword: '',
+			confirmPassword: '',
+		},
+	});
+
+	const onResetPasswordFormSubmit = async (data: z.infer<typeof ResetPasswordFormSchema>) => {
+		setError(null);
+		setIsLoading(true);
+		const token = searchParams.get('token');
+
+		if (!token) {
+			setError('No reset token found. Please request a new reset link.');
+			setIsLoading(false);
+			return;
+		}
+
+		try {
+			await apiClient.authApi.resetPassword({ resetPasswordDto: { token, newPassword: data.newPassword } });
+			router.push(ROUTES.LOGIN);
+		} catch (error) {
+			setError('An error occurred while resetting your password. Please try again.');
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return (
+		<Form {...resetPasswordForm}>
+			<form onSubmit={resetPasswordForm.handleSubmit(onResetPasswordFormSubmit)} className="w-full flex flex-col gap-4">
+				<FormField
+					control={resetPasswordForm.control}
+					name="newPassword"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>New Password</FormLabel>
+							<FormControl>
+								<Input type="password" {...field} />
+							</FormControl>
+							<FormDescription className="text-red-700">
+								{resetPasswordForm.formState.errors.newPassword?.message}
+							</FormDescription>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={resetPasswordForm.control}
+					name="confirmPassword"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Confirm New Password</FormLabel>
+							<FormControl>
+								<Input type="password" {...field} />
+							</FormControl>
+							<FormDescription className="text-red-700">
+								{resetPasswordForm.formState.errors.confirmPassword?.message}
+							</FormDescription>
+						</FormItem>
+					)}
+				/>
+				{error && <FormDescription className="text-red-700 mb-2">{error}</FormDescription>}
+				<div className="w-full flex flex-row justify-end gap-2">
+					<Button type="submit" disabled={isLoading}>
+						{isLoading ? 'Resetting...' : 'Reset Password'}
+					</Button>
+					<Link href={ROUTES.HOME}>
+						<Button variant={'secondary'} disabled={isLoading}>
+							Cancel
+						</Button>
+					</Link>
+				</div>
+			</form>
+		</Form>
+	);
 }
