f48d8523edc11fd013a76e8b7ad3a06ff348dfc0
Micael Jerry <fidimalala.mj@gmail.com>
2025-10-06 14:07:38 +0300

    fix: update schema

--- src/schema/auth/reset-password-request-form-schema.ts
+++ src/schema/auth/reset-password-request-form-schema.ts
@@ -1,5 +1,5 @@
 import z from 'zod';
 
 export const ResetPasswordRequestFormSchema = z.object({
-	email: z.email().min(1, 'Email is required'),
+	email: z.email('Invalid email address').min(1, 'Email is required'),
 });
--- src/schema/auth/signup-form-schema.ts
+++ src/schema/auth/signup-form-schema.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 export const SignupFormSchema = z
 	.object({
 		username: z.string().min(5, 'Username is required'),
-		email: z.email().min(1, 'Email is required'),
+		email: z.email('Invalid email address').min(1, 'Email is required'),
 		password: z
 			.string()
 			.min(8, 'Password must be at least 8 characters long')
